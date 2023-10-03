f3fe2cf95e3219d5d8f7e0e114e8a0915ad4c11f
Vasily Maximov <softservice-31@mail.ru>
2023-10-03 18:16:28 +0300

    Error Maven

--- src/test/java/ru/practicum/shareit/user/controller/UserControllerTest.java
+++ src/test/java/ru/practicum/shareit/user/controller/UserControllerTest.java
@@ -24,6 +24,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 import static org.hamcrest.Matchers.is;
 
+
 @WebMvcTest(controllers = UserController.class)
 class UserControllerTest {
 
